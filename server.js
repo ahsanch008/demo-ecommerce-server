004d8d8b1a654672accb19324cedad34da5ac71a
ahsanch008 <ahsan788766@gmail.com>
2024-09-22 19:44:58 +0500

    ..

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://urbanstore-ecommerce.vercel.app/'],
   credentials: true
 }));
 
